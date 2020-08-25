130717ea16b14ea2a3b9f6b61f28dbd6aa3240ad
Katrioska <48627712+Katrioska@users.noreply.github.com>
2020-08-24 20:31:18 -0400

    Update ra2.py

--- src/src/ra2.py
+++ src/src/ra2.py
@@ -1,8 +1,8 @@
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
 from base64 import b64encode, b64decode
-from os import remove
-from pickle import dumps, loads
+from os import remove, path
+from pickle import dumps, loads, dump, load
 
 class RandomEncryptor2:
 	def __init__(self):
@@ -31,6 +31,30 @@ class RandomEncryptor2:
 		todecrypt = loads(bytearray.fromhex(data))
 		cipher = AES.new(self.__key, AES.MODE_CFB, iv=todecrypt["iv"])
 		return cipher.decrypt(todecrypt["data"]).decode()
+
+	def encryptFile(self, pathFile, savePath):
+		cipher = AES.new(self.__key, AES.MODE_CFB)
+		with open(pathFile, "rb") as f:
+			cipher_data = cipher.encrypt(f.read())
+
+		file_name = pathFile.split('\\')[-1]
+		file_extension = pathFile.split(".")[-1]
+		data_to_dump = [file_name, cipher_data.hex()]
+		
+		with open(savePath+"\\"+file_name.replace("."+file_extension, ".bin"), "wb") as ff:
+			dump(data_to_dump, ff)
+
+		remove(pathFile)
+
+	def decryptFile(self, pathFile, savePath):
+		cipher = AES.new(self.__key, AES.MODE_CFB)
+		with open(pathFile, "rb") as f:
+			data_to_load = load(f)
+
+		with open(savePath+"\\"+data_to_load[0], "wb") as fd:
+			fd.write(cipher.decrypt(bytearray.fromhex(data_to_load[1])))
+
+		remove(pathFile)
 
 	def generateKey(self):
 		return get_random_bytes(32)
